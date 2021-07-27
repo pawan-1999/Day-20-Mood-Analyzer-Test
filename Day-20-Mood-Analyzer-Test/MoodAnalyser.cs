cafcc52d6235e0e200c6307faaf1569086cd94b1
pawan-1999 <guptapawan767@gmail.com>
2021-07-27 23:40:37 +0530

    Added : Mood Analyzer Method To Check Mood

--- Day-20-Mood-Analyzer-Test/MoodAnalyser.cs
+++ Day-20-Mood-Analyzer-Test/MoodAnalyser.cs
@@ -16,12 +16,21 @@ namespace Mood_Analyser
         }
         public string analyseMood()
         {
-            if (this.message.Contains("sad"))
-                return "SAD";
-            else if (this.message.Contains("I"))
-                return "SAD";
-            else
-                return "HAPPY";
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
         }
     }
 
