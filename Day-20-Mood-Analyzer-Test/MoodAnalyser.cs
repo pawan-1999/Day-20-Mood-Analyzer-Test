f6a13c71a093ac85885bca239e8b96b2a08e6667
pawan-1999 <guptapawan767@gmail.com>
2021-07-27 23:00:31 +0530

    Added : Checking Mood By MoodAnalyser

---
+++ Day-20-Mood-Analyzer-Test/MoodAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mood_Analyser
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser()
+        {
+        }
+        public string analyseMood()
+        {
+              if (this.message.Contains("sad"))
+                    return "SAD";
+              else
+                    return "HAPPY";
+        }
+    }
+
+}
